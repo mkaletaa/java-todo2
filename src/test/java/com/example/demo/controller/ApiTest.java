38cbca88245eb88c679cce67e22407b85e28ca10
mkaletaa <kaleta222@gmail.com>
2023-01-29 10:51:09 +0100

    api test

--- src/test/java/com/example/demo/controller/ApiTest.java
+++ src/test/java/com/example/demo/controller/ApiTest.java
@@ -17,6 +17,7 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -40,7 +41,6 @@ public class ApiTest {
 
         //then
 
-
         // Assert that the response has a status of OK
         assertEquals(HttpStatus.OK, response.getStatusCode(), "Http status should be OK");
 
@@ -49,33 +49,34 @@ public class ApiTest {
 //        assertThat(response.getBody()).hasSize(13);
     }
 
-//    @Test
 
-//    @Test
-//    public void shouldPost() {
-//        // Create a new Task object
-//        Task task = new Task(1, "nazwa", "opis");
-//
-//        // Make a POST request to the endpoint
-//        ResponseEntity<String> response = restTemplate.postForEntity("/tasks", task, String.class);
-//
-//        // Assert that the response has a status of CREATED
-//        assertEquals(HttpStatus.OK, response.getStatusCode(), "Http status should be OK");
-//
-//        // Assert that the response body is not null
-//        assertNotNull(response.getBody());
-//
-//        // Add another test to check if the task is added to the mongodb or not
-//        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-//        MongoDatabase db = mongoClient.getDatabase("mydb");
-//        MongoCollection<Document> collection = db.getCollection("tasks");
-//        Document taskToCheck = collection.find(new Document("id", task.getId())).first();
-//
-//        assertNotNull(taskToCheck);
-//        assertEquals(task.getId(), taskToCheck.get("id"));
-//        assertEquals(task.getName(), taskToCheck.get("name"));
-//        assertEquals(task.getDescription(), taskToCheck.get("description"));
-//    }
+
+    @Test
+    public void shouldPost() {
+        // Create a new Task object
+        UUID task1Id = UUID.fromString("e499b5df-e341-41c5-bf7a-06bc9c9bc4e9");
+        Task task = new Task(task1Id, "dede", "ddwww");
+
+        // Make a POST request to the endpoint
+        ResponseEntity<String> response = restTemplate.postForEntity("/tasks", task, String.class);
+
+        // Assert that the response has a status of CREATED
+        assertEquals(HttpStatus.OK, response.getStatusCode(), "Http status should be OK");
+
+        // Assert that the response body is not null
+        assertNotNull(response.getBody());
+
+        // Add another test to check if the task is added to the mongodb or not
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase db = mongoClient.getDatabase("mydb");
+        MongoCollection<Document> collection = db.getCollection("tasks");
+        Document taskToCheck = collection.find(new Document("id", task.getId())).first();
+
+        assertNotNull(taskToCheck);
+        assertEquals(task.getId(), taskToCheck.get("id"));
+        assertEquals(task.getName(), taskToCheck.get("name"));
+        assertEquals(task.getDescription(), taskToCheck.get("description"));
+    }
 
 
 }
